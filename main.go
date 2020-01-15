3792e008d090c47c6e2d43ae6ac9dd6a60cd34cb
Levin <xlw712@163.com>
2020-01-15 11:10:42 +0800

    add test code

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"container/list"
 	"flag"
 	"fmt"
 	"strconv"
+	"sync"
 )
 
 var levin int
@@ -48,17 +50,103 @@ func main() {
 	// 使用指针交换两个变量
 	var x, y = 1, 2
 	swap(&x, &y)
+	// 接收启动参数 go run --sql "select * from t"
 	var mode = flag.String("sql", "", "process mode")
 	flag.Parse()
 	fmt.Println(*mode)
+	// 定义枚举类型
 	var day = Weekday(Sunday)
-	fmt.Println(day.String())
+	fmt.Printf("%T \n", day)
+	// 构造数组 遍历数组
+	var arrInt = []int{1, 2, 3, 4}
+	// 使用切片获取数据 数据3,4 下标从0开始 前包含后不包含
+	fmt.Println(arrInt[2:])
+	// 向数组中追加数据
+	arrInt = append(arrInt, 6, 7, 8)
+	// 合并两个切片注意一定要加...
+	arrInt = append(arrInt[0:2], arrInt[3:7]...)
+	for k, v := range arrInt {
+		fmt.Println(k, v, cap(arrInt))
+	}
+	//初始化map
+	var map1 map[string]string
+	//如果不执行make会报错,因为map1是一个Nil
+	map1 = make(map[string]string, 10)
+	map1["name"] = "xiaoliwen"
+	map1["country"] = "china"
+	if _, ok := map1["noExists"]; ok == false {
+		fmt.Println("没找到key")
+	}
+	delete(map1, "name")
+	for key, value := range map1 {
+		fmt.Println(key, value)
+	}
+	//初始化syncmap
+	conMap := new(sync.Map)
+	conMap.Store("who is Map type?", "I'am concurrent Map")
+	conMap.Store("I'am crazy", "yes")
+	fmt.Println(conMap.Load("who is Map type?"))
+	conMap.Range(func(key, value interface{}) bool {
+		fmt.Println(key, "->", value)
+		return true
+	})
+	//初始化list
+	var list = list.New()
+	list.PushBack("A")
+	list.PushFront("B")
+	list.PushBack("C")
+	for i := list.Front(); i != nil; i = i.Next() {
+		fmt.Println(i.Value)
+	}
+	//nil值
+	var a1 string
+	var a2 string
+	fmt.Println(a1 == a2)
+	// if
+	if _, error := strconv.Atoi("9207d12"); error != nil {
+		fmt.Println(error)
+	}
+	//遍历通道接收数据
+	c := make(chan int)
+	go func() {
+		c <- 1
+		c <- 2
+		c <- 3
+		close(c)
+	}()
+	for v := range c {
+		fmt.Println(v)
+	}
+	//switch
+	str1 := "hello"
+	switch {
+	case str1 == "hello":
+		fmt.Println(str1)
+		fallthrough
+	case str1 != "xiaoliwen":
+		fmt.Println("xiaoliwen")
+	}
+	// goto
+	tmp_count := 10
+	for {
+		tmp_count--
+		if tmp_count == 0 {
+			goto exit
+		}
+	}
+exit:
+	{
+		fmt.Println("退出循环")
+	}
+
+	var chinese = "我是中国人， I am Chinese"
+	fmt.Println([]rune(chinese))
 }
 
 type Weekday int
 
 const (
-	Sunday = 2 << iota
+	Sunday Weekday = 2 << iota
 	Monday
 	Tuesday
 	Wednesday
@@ -67,6 +155,9 @@ const (
 	Saturday
 )
 
+func getWeek(week Weekday) {
+	fmt.Println(week)
+}
 func (w Weekday) String() string {
 	switch w {
 	case Sunday:
@@ -77,15 +168,12 @@ func (w Weekday) String() string {
 	return "N/A"
 }
 
-func getWeek(week Weekday) {
-	fmt.Println(week)
-}
-
 type IntSlice []int
 
 func (p IntSlice) Len() int           { return len(p) }
 func (p IntSlice) Less(i, j int) bool { return p[i] < p[j] }
 func (p IntSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p IntSlice) add(v int)          { p = append(p, v) }
 
 type Person struct {
 	name    string
