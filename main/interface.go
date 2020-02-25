cce827b5ee8fb28e59ffbbde30adfb6368499be4
mac <mac@macdeMacBook-Pro.local>
2020-02-25 20:15:41 +0800

    commit

--- main/interface.go
+++ main/interface.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"sort"
 )
@@ -43,6 +44,13 @@ func (s Students) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
+type CustomerError struct {
+}
+
+func (*CustomerError) Error() string {
+	return "这是一个自定义异常"
+}
+
 func main() {
 	var cat Cat
 	cat = new(BlackCat)
@@ -70,8 +78,13 @@ func main() {
 	sort.Slice(sliceStudent, func(i, j int) bool {
 		return sliceStudent[i].age < sliceStudent[j].age
 	})
-	for _, v := range sliceStudent{
+	for _, v := range sliceStudent {
 		fmt.Printf("%+v\n", v)
 	}
+	// 异常接口实例
+	var error error
+	error = new(CustomerError)
+	fmt.Printf("这个异常的信息是:"+error.Error())
 
+	errors.New("new Error mode")
 }
